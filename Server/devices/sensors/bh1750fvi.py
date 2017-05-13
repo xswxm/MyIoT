f2c9c74f94b644e005462bb49e00549805850e0d
xswxm <hzxswxm@gmail.com>
2017-05-13 23:59:10 +1000

    Initial commit

--- Server/devices/sensors/bh1750fvi.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-# -*- encoding: utf-8 -*-
-
-import smbus
-def get(addr):
-    bus = smbus.SMBus(1)
-    data = bus.read_i2c_block_data(addr,0x11)
-    return (data[1] + (256 * data[0])) / 1.2
